efef4ec8dbd0f07b241b2e65eca6661fa038fec0
Roch Goszczyński <roch.goszczynski@gmail.com>
2019-12-07 15:10:59 +0100

    Add day 7 folder

---
+++ Day7/Puzzle.cs
@@ -0,0 +1,11 @@
+
+namespace AdventOfCode.Day7
+{
+    public class Puzzle1 : AdventOfCode.Day5.Puzzle
+    {
+        public override int GetResult()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
